42aa2280675c9f14e43da1b89c78f8b98bc61e15
KhangHaha <92001682@students.sd308.org>
2024-10-15 18:22:32 -0500

    step5 fix

--- src/main/java/team/gif/robot/subsystems/SparkController.java
+++ src/main/java/team/gif/robot/subsystems/SparkController.java
@@ -1,12 +1,13 @@
 package team.gif.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team.gif.robot.RobotMap;
 
 public class SparkController extends SubsystemBase {
 
-    CANSparkMax sparky = new CANSparkMax(RobotMap.SPARK_MAX_ID, CANSparkMax.MotorType.kBrushless);
+    CANSparkMax sparky = new CANSparkMax(RobotMap.SPARK_MAX_ID, CANSparkLowLevel.MotorType.kBrushless);
     public SparkController(){
     }
     public void setSparky(double percent){
