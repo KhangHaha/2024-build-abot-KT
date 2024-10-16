5c247d625f7b496715e81933c5a78a52c216b349
KhangHaha <92001682@students.sd308.org>
2024-10-15 19:25:12 -0500

    talonsrx fixes

--- src/main/java/team/gif/robot/subsystems/TalonSRXMotor.java
+++ src/main/java/team/gif/robot/subsystems/TalonSRXMotor.java
@@ -1,6 +1,7 @@
 package team.gif.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team.gif.robot.RobotMap;
@@ -11,7 +12,8 @@ public class TalonSRXMotor extends SubsystemBase {
 
     public TalonSRXMotor() {
         motor = new TalonSRX(RobotMap.TALON_ID);
-
+        motor.configFactoryDefault();
+        motor.setNeutralMode(NeutralMode.Brake);
     }
 
 
