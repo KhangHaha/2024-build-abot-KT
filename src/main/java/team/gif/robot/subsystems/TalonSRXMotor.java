f2b82ca8f79c4258ebb9f4ad1c708dbe64c28d5a
KhangHaha <92001682@students.sd308.org>
2024-10-17 16:58:04 -0500

    talon srx format

--- src/main/java/team/gif/robot/subsystems/TalonSRXMotor.java
+++ src/main/java/team/gif/robot/subsystems/TalonSRXMotor.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team.gif.robot.RobotMap;
 
 public class TalonSRXMotor extends SubsystemBase {
-    public static double output = .2;
+   
     private final TalonSRX motor;
 
     public TalonSRXMotor() {
